d58d9bccb5a2afebe30ad3853c9115f1366f67fe
Christina Bui <uyen.buitie@gmail.com>
2024-02-25 17:45:59 -0600

    Optimize

--- src/Components/Pages/Open.js
+++ src/Components/Pages/Open.js
@@ -13,8 +13,8 @@ class Open extends Component {
                 
                 <div className='text'>
         
-                    <Fade down duration = {2000}><h1>We are Open Everyday</h1></Fade>
-                    <p>Monday - Sunday</p>
+                    <h1 style={{marginBottom: "-50px"}}>We are Open Everyday</h1>
+                    <p style={{marginBottom: "-20px"}}>Monday - Sunday</p>
                     <h2>10:30 AM–7:30 PM</h2>
                     <button onClick={this.handleClick} className='red-button'>Visit Us</button>
                 
